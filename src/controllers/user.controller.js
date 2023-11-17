9286c1c7ca06198b94c08bd2a8a276913beecf9e
marcelo-objtcv <marcelo@objctv.one>
2023-11-16 21:03:16 -0300

    Create the logic  for wuestions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -127,15 +127,15 @@ exports.saveAnswer = async (req, res) => {
   try {
     console.log('saveAnswer called'); 
 
-    // const userId = req.user._id;
+    const userId = '1';
     const { id, correct, responseTime, topic } = req.body;
 
-    // console.log('Request body:', req.body); // log the request body
+    console.log('Request body:', req.body); // log the request body
 
-    // const user = await User.findById(userId);
-    // if (!user) {
-    //   return res.status(404).json({ message: 'User not found' });
-    // }
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
 
     // const answerIndex = user.answers.findIndex(answer => answer.topic === topic);
     // const answerDetail = { id, correct, responseTime };
